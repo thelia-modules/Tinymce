085245c62cc4aba4b0e3408914a3c9aadb571b4a
Alexandre Nozière <anoziere@openstudio.fr>
2026-04-22 11:04:22 +0200

    fix: register module classes as autowired services

--- Tinymce.php
+++ Tinymce.php
@@ -13,6 +13,7 @@
 namespace Tinymce;
 
 use Propel\Runtime\Connection\ConnectionInterface;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ServicesConfigurator;
 use Symfony\Component\Filesystem\Filesystem;
 use Thelia\Action\Document;
 use Thelia\Model\ConfigQuery;
@@ -95,5 +96,13 @@ class Tinymce extends BaseModule
 
             $fileSystem->remove($this->webMediaPath);
         }
+    }
+
+    public static function configureServices(ServicesConfigurator $servicesConfigurator): void
+    {
+        $servicesConfigurator->load(self::getModuleCode().'\\', __DIR__)
+            ->exclude([__DIR__.'/I18n/*'])
+            ->autowire(true)
+            ->autoconfigure(true);
     }
 }
